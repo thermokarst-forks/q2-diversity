11c748df20fa1ecc23c7fd2ce4c1592abb182156
Greg Caporaso <gregcaporaso@gmail.com>
2016-05-19 12:13:32 -0700

    TST: fixes #11

--- q2_diversity/tests/test_beta.py
+++ q2_diversity/tests/test_beta.py
@@ -5,3 +5,69 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
+
+import unittest
+import io
+
+import skbio
+import numpy as np
+import numpy.testing as npt
+from biom.table import Table
+
+from q2_diversity import beta_diversity
+
+
+class BetaDiversityTests(unittest.TestCase):
+
+    def test_non_phylogenetic(self):
+        t = Table(np.array([[0, 1, 3], [1, 1, 2]]),
+                  ['O1', 'O2'],
+                  ['S1', 'S2', 'S3'])
+        actual = beta_diversity('braycurtis', t)
+        # expected computed with scipy.spatial.distance.braycurtis
+        expected = skbio.DistanceMatrix([[0.0000000, 0.3333333, 0.6666667],
+                                         [0.3333333, 0.0000000, 0.4285714],
+                                         [0.6666667, 0.4285714, 0.0000000]],
+                                        ids=['S1', 'S2', 'S3'])
+
+        self.assertEqual(actual.ids, expected.ids)
+        for id1 in actual.ids:
+            for id2 in actual.ids:
+                npt.assert_almost_equal(actual[id1, id2], expected[id1, id2])
+
+    def test_phylogenetic(self):
+        t = Table(np.array([[0, 1, 3], [1, 1, 2]]),
+                  ['O1', 'O2'],
+                  ['S1', 'S2', 'S3'])
+        tree = skbio.TreeNode.read(io.StringIO(
+            '((O1:0.25, O2:0.50):0.25, O3:0.75)root;'))
+        actual = beta_diversity('unweighted_unifrac', t, phylogeny=tree)
+        # expected computed with skbio.diversity.beta_diversity
+        expected = skbio.DistanceMatrix([[0.00, 0.25, 0.25],
+                                         [0.25, 0.00, 0.00],
+                                         [0.25, 0.00, 0.00]],
+                                        ids=['S1', 'S2', 'S3'])
+
+        self.assertEqual(actual.ids, expected.ids)
+        for id1 in actual.ids:
+            for id2 in actual.ids:
+                npt.assert_almost_equal(actual[id1, id2], expected[id1, id2])
+
+    def test_non_phylogenetic_invalid_input(self):
+        t = Table(np.array([[0, 1, 3], [1, 1, 2]]),
+                  ['O1', 'O2'],
+                  ['S1', 'S2', 'S3'])
+        tree = skbio.TreeNode.read(io.StringIO(
+            '((O1:0.25, O2:0.50):0.25, O3:0.75)root;'))
+        with self.assertRaises(TypeError):
+            beta_diversity('bray_curtis', t, phylogeny=tree)
+
+    def test_phylogenetic_invalid_input(self):
+        t = Table(np.array([[0, 1, 3], [1, 1, 2]]),
+                  ['O1', 'O2'],
+                  ['S1', 'S2', 'S3'])
+        with self.assertRaises(TypeError):
+            beta_diversity('unweighted_unifrac', t)
+
+if __name__ == "__main__":
+    unittest.main()

3e9612ffe8b276906dfea22168d6911a99b28b58
Matthew Dillon <matthewrdillon@gmail.com>
2017-04-12 10:08:54 -0700

    MAINT: template recipe

--- setup.py
+++ setup.py
@@ -15,14 +15,6 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
-    install_requires=['qiime2 == 2017.3.*', 'q2-feature-table == 2017.3.*',
-                      'q2-types == 2017.3.*', 'q2templates == 2017.3.*',
-                      'scikit-bio', 'seaborn', 'statsmodels', 'scipy', 'numpy',
-                      'pandas', 'biom-format >= 2.1.5, < 2.2.0',
-                      # `ipywidgets` included to avoid ShimWarning from
-                      # `seaborn` imports:
-                      #  https://github.com/mwaskom/seaborn/issues/874
-                      'ipywidgets'],
     package_data={'q2_diversity._alpha': [
                       'alpha_group_significance_assets/index.html',
                       'alpha_group_significance_assets/dst/*',
